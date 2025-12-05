f34afaaa33db15bf197a82ccecf0995db7a58f80
someone55interesting <korolperehodov9@gmail.com>
2025-12-05 22:32:59 +0300

    Update wsgi.py

--- amazon_clone/wsgi.py
+++ amazon_clone/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'amazon_clone.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles'))
